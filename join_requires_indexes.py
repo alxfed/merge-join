927129e6b1dc0aeb011b4d74e721db560c1eac6c
alxfed <alxfed@gmail.com>
2020-01-05 16:59:45 -0600

    set_index

--- join_requires_indexes.py
+++ join_requires_indexes.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
-"""...
+"""https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.set_index.html#pandas.DataFrame.set_index
 """
 import pandas as pd
 
 
 def main():
+    data_with_column_names = pd.DataFrame(
+        {'a': [1, 2, 3],
+         'b': [4, 5, 6],
+         'c': [7, 8, 9]}
+    )
+    data = data_with_column_names.set_index(keys='a', drop=False, verify_integrity=False)
+    # drop means delete the column used for index creation
     print('ok')
     return
 
