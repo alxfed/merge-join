1a3f114bcb9f79e5fae7c4f719b51774df7bafa4
alxfed <alxfed@gmail.com>
2020-01-05 17:27:04 -0600

    selection by callable

--- selection_by_callable.py
+++ selection_by_callable.py
@@ -3,9 +3,15 @@
 https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html?#selection-by-callable
 """
 import pandas as pd
+import numpy as np
 
 
 def main():
+    data = pd.DataFrame(np.random.randn(6, 4),
+                        index = list('abcdef'),
+                        columns = list('ABCD'))
+    indexed = data.loc[lambda df: df.A > 0, :]
+    print('ok')
     return
 
 
