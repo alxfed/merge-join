06a8142b315e1af1065e710a5ec25bbfe3c2d747
alxfed <alxfed@gmail.com>
2020-01-06 08:33:45 -0600

    reindexing

---
+++ index_objects.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""...
+"""
+import pandas as pd
+import numpy as np
+
+
+def main():
+    data_with_row_and_column_names = pd.DataFrame(
+        [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 0]], index=['line_1', 'line_3', 'line_2'], columns= ['col_1', 'col_3', 'col_2']
+    )
+
+    cols = pd.Index(['col_1', 'col_2', 'col_3'], dtype=str, name='columns')
+    inde = pd.Index(['line_1', 'line_2', 'line_3'], dtype=str, name='lines')
+    result = data_with_row_and_column_names.reindex(index=inde, columns=cols)
+    print('ok')
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
