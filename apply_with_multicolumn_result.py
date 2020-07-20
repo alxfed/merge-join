e13282fd06771c426e28256e06fca22b7075c100
alxfed <alxfed@gmail.com>
2020-07-20 10:00:44 -0500

    reindexing

--- apply_with_multicolumn_result.py
+++ apply_with_multicolumn_result.py
@@ -7,20 +7,21 @@ from numpy.random import randn
 
 
 def multicolumn(row):
-    a = row['index'].month
-    b = row['index'].day
-    c = row['index'].hour
-    return pd.Series([a, b, c])
+    month = row['index'].month
+    day = row['index'].day
+    hour = row['index'].hour
+    # https://docs.python.org/3/tutorial/inputoutput.html?highlight=numbers%20output%20formatting
+    mm = f'{month:02d}'
+    dd = f'{day:02d}'
+    hh = f'{hour:02d}'
+    return pd.Series([mm, dd, hh, month, day, hour])
 
 
 def main():
     dti = pd.date_range(start='2020-04-21 20:00:00', end=datetime.now(), freq='H')
-    # sse = dti.to_series(name='daytime')
-    # dt = list(dti)
     data = pd.DataFrame(randn(len(dti), 2), columns=['rand1', 'rand2'], index=dti)
     data.reset_index(drop=False, inplace=True)
-    # se = data.index.to_series()
-    data[['mm', 'dd', 'hh']] = data.apply(multicolumn, axis='columns')
+    data[['mm', 'dd', 'hh', 'month', 'day', 'hour']] = data.apply(multicolumn, axis='columns')
     data.drop(columns=['rand1', 'rand2'], inplace=True)
     return
 
